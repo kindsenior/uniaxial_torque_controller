fb0a2961841c6509ca4b7e88850eeb9dc5cd8d82
kindsenior <kunio.einstein@gmail.com>
2021-01-07 15:02:30 +0900

    print usage in print-reference-trajectory-data.sh

--- print-reference-trajectory-data.sh
+++ print-reference-trajectory-data.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then
+    echo "[Error] Useage ./print-reference-trajectory-data.sh <reference-trajectory-file-name.l>"
+    exit
+fi
+
 roseus "(load \"reference-trajectory-generator.l\")" "(load \"$1\")" "(send *tg* :print-param)" 2>/dev/null
